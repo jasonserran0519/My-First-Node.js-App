c350f29110c1f35f59172e606df3bbd7d23e8770
jasonserran0519 <146367145+jasonserran0519@users.noreply.github.com>
2024-11-12 18:28:15 -0800

    added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
 
 const app = new Hono()
-
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.use ('/*', serveStatic({
+  root: './static',
+}))
+
 
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
